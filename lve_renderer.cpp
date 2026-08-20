cf213b8b4c9cc8319baae6cb9d6af1fbddeabf2c
羊肉汤 <450477095@qq.com>
2026-08-20 15:56:40 +0800

    Add files via upload

---
+++ lve_renderer.cpp
@@ -0,0 +1,227 @@
+﻿#include"lve_renderer.h"
+
+namespace lve {
+	void LveRenderer::createCommandBuffers(VkDevice device, VkCommandPool commandPool, uint32_t commandBufferCount) {
+
+		//创建多个命令缓冲区
+		commandBuffers.resize(commandBufferCount);
+		VkCommandBufferAllocateInfo allocInfo{};//命令缓冲区分配信息
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;//命令缓冲区分配信息
+		allocInfo.commandPool = commandPool;//命令池
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;//主命令缓冲区[指定分配主的还是辅助的]
+		allocInfo.commandBufferCount = (uint32_t)commandBuffers.size();//分配size个命令缓冲区
+		if (vkAllocateCommandBuffers(device, &allocInfo, commandBuffers.data()) != VK_SUCCESS) {//分配命令缓冲区
+			throw std::runtime_error("failed to allocate command buffers!");
+		}
+
+	}
+	void LveRenderer::createSignalSemaphore(VkDevice device,uint32_t swapChainImageCount) {
+		//创建多个信号量和栅栏
+		imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
+		renderFinishedSemaphores.resize(swapChainImageCount);//根据
+		inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
+		VkSemaphoreCreateInfo semaphoreInfo{};//信号量信息
+		semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;//信号量信息
+
+		VkFenceCreateInfo fenceInfo{};//栅栏信息
+		fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+		fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;//栅栏创建时就处于信号状态,这样第一次渲染的时候就不会阻塞
+
+		// 每个“飞行帧”一个：图片可用信号量和 Fence
+		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+			if (vkCreateSemaphore(
+				device,
+				&semaphoreInfo,
+				nullptr,
+				&imageAvailableSemaphores[i]) != VK_SUCCESS ||
+				vkCreateFence(
+					device,
+					&fenceInfo,
+					nullptr,
+					&inFlightFences[i]) != VK_SUCCESS) {
+
+				throw std::runtime_error("failed to create frame sync objects!");
+			}
+		}
+
+		// 每张交换链图片一个：渲染完成信号量
+		for (size_t i = 0; i < swapChainImageCount; i++) {
+			if (vkCreateSemaphore(
+				device,
+				&semaphoreInfo,
+				nullptr,
+				&renderFinishedSemaphores[i]) != VK_SUCCESS) {
+
+				throw std::runtime_error("failed to create render finished semaphore!");
+			}
+		}
+	}
+	void LveRenderer::run(VkDevice device, LveSwapChain& swapChain, VkQueue graphicsQueue, VkQueue presentQueue, uint32_t& currentFrame,
+		VkRenderPass renderPass, LveModel& model,const std::vector<VkDescriptorSet> descriptorSets, VkPipelineLayout pipelineLayout,LveUniform uniform,
+		const glm::mat4 modelMatirx,const glm::mat4 view,const glm::mat4 proj,LveCompute& compute) {
+		vkWaitForFences(device, 1, &inFlightFences[currentFrame], VK_TRUE, UINT64_MAX);//等待栅栏,直到渲染完成
+		vkAcquireNextImageKHR(device, swapChain.getSwapChain(), UINT64_MAX, imageAvailableSemaphores[currentFrame], VK_NULL_HANDLE, &imageIndex);//获取下一张交换链图片的索引,并将imageAvailableSemaphore信号量设置为在图像可用时发出信号
+		vkResetFences(device, 1, &inFlightFences[currentFrame]);//重置栅栏,以便下一次使用
+		//获取交换链图像
+		uniform.updateUniformBuffer(currentFrame, swapChain.getSwapChainExtent(), modelMatirx, view,proj);//更新上传ubo
+	
+		vkResetCommandBuffer(commandBuffers[currentFrame], 0);//重置命令缓冲区,以便重新记录命令缓冲区
+		recordCommandBuffer(commandBuffers[currentFrame], imageIndex,
+			renderPass,swapChain.getSwapChainFrameBuffer(imageIndex),swapChain.getSwapChainExtent(),model,pipelineLayout,currentFrame,descriptorSets,compute);//记录命令缓冲区
+
+		//提交命令缓冲区
+		VkSubmitInfo submitInfo{};//提交信息
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+
+		VkSemaphore waitSemaphores[] = { imageAvailableSemaphores[currentFrame] };//等待的信号量
+		VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };//等待的阶段
+		submitInfo.waitSemaphoreCount = 1;//等待的信号量数量
+		submitInfo.pWaitSemaphores = waitSemaphores;//等待的信号量
+		submitInfo.pWaitDstStageMask = waitStages;//等待的阶段
+		submitInfo.commandBufferCount = 1;//提交的命令缓冲区数量
+		submitInfo.pCommandBuffers = &commandBuffers[currentFrame];//提交的命令缓冲区
+
+		VkSemaphore signalSemaphores[] = { renderFinishedSemaphores[imageIndex] };//执行完毕后触发的信号量
+		submitInfo.signalSemaphoreCount = 1;//信号量数量
+		submitInfo.pSignalSemaphores = signalSemaphores;//信号量
+		if (vkQueueSubmit(graphicsQueue, 1, &submitInfo, inFlightFences[currentFrame]) != VK_SUCCESS) {//提交命令缓冲区
+			throw std::runtime_error("failed to submit draw command buffer!");
+		}
+
+
+		//提交渲染结果
+		//提交回交换链
+		VkPresentInfoKHR presentInfo{};//提交信息
+		presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;//提交信息
+
+		presentInfo.waitSemaphoreCount = 1;//等待的信号量数量
+		presentInfo.pWaitSemaphores = signalSemaphores;//等待的信号量
+		VkSwapchainKHR swapChains[] = { swapChain.getSwapChain()};//交换链
+		presentInfo.swapchainCount = 1;//交换链数量
+		presentInfo.pSwapchains = swapChains;//交换链
+		presentInfo.pImageIndices = &imageIndex;//交换链图像索引
+		presentInfo.pResults = nullptr;//结果
+		vkQueuePresentKHR(presentQueue, &presentInfo);//提交回交换链
+
+		currentFrame = (currentFrame + 1) % MAX_FRAMES_IN_FLIGHT;//前进刀下一帧
+	};
+
+	void LveRenderer::recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex,VkRenderPass renderPass,
+		VkFramebuffer framebuffer,VkExtent2D extent,LveModel& model, VkPipelineLayout pipelineLayout,uint32_t currentFrame,
+		const std::vector<VkDescriptorSet> descriptorSets, LveCompute& compute) {
+		{
+			VkCommandBufferBeginInfo beginInfo{};
+			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;//~
+			beginInfo.flags = 0;//使用的行为默认
+			beginInfo.pInheritanceInfo = nullptr;//用于辅助缓冲区,继承主缓冲区时用的
+
+			if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS) {//开始记录命令缓冲区
+				throw std::runtime_error("failed to begin recording command buffer!");
+			}
+		
+			//启动渲染通道
+			VkRenderPassBeginInfo renderPassBeginInfo{};//渲染通道开始信息
+			renderPassBeginInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;//渲染通道开始信息
+			renderPassBeginInfo.renderPass = renderPass;//渲染通道
+			renderPassBeginInfo.framebuffer = framebuffer;//帧缓冲区
+			renderPassBeginInfo.renderArea.offset = { 0, 0 };//渲染区域偏移量
+			renderPassBeginInfo.renderArea.extent = extent;//渲染区域大小
+
+			VkClearValue clearValues[2]{};
+
+			clearValues[0].color = {
+				{0.0f, 0.0f, 0.0f, 1.0f}
+			};
+
+			clearValues[1].depthStencil = {
+				1.0f,
+				0
+			};
+
+			renderPassBeginInfo.clearValueCount = 2;
+			renderPassBeginInfo.pClearValues = clearValues;
+
+
+			vkCmdBeginRenderPass(commandBuffer, &renderPassBeginInfo, VK_SUBPASS_CONTENTS_INLINE);//开始渲染通道
+
+			//视图暂时用静态的,后面while再加动态
+			VkViewport viewport{};
+			viewport.x = 0.0f;
+			viewport.y = 0.0f;
+			viewport.width = static_cast<float>(extent.width);
+			viewport.height = static_cast<float>(extent.height);
+			viewport.minDepth = 0.0f;
+			viewport.maxDepth = 1.0f;
+			vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+			VkRect2D scissor{};
+			scissor.offset = { 0, 0 };
+			scissor.extent = extent;
+			vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+
+			//绑定图像管线
+			vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);//绑定图像管线
+			model.bindVertex(commandBuffer);//绑定顶点
+			model.bindIndexBuffer(commandBuffer);//绑定索引缓冲
+			vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSets[currentFrame], 0, nullptr);//绑定描述符集
+			//绘制三角形
+			model.draw_index_mode(commandBuffer);
+
+			//结束渲染通道
+			vkCmdEndRenderPass(commandBuffer);
+
+			if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {//结束命令缓冲区
+				throw std::runtime_error("failed to record command buffer!");
+			}
+
+		}
+	};
+
+	void LveRenderer::clean(VkDevice device) {
+		if (graphicsPipeline != VK_NULL_HANDLE) {
+			vkDestroyPipeline(
+				device,
+				graphicsPipeline,
+				nullptr
+			);
+
+			graphicsPipeline = VK_NULL_HANDLE;
+		}
+
+		// 有多少个就销毁多少个
+		for (VkSemaphore semaphore :
+		renderFinishedSemaphores) {
+
+			vkDestroySemaphore(
+				device,
+				semaphore,
+				nullptr
+			);
+		}
+
+		renderFinishedSemaphores.clear();
+
+		// 这些按飞行帧数量销毁
+		for (size_t i = 0;
+			i < MAX_FRAMES_IN_FLIGHT;
+			++i) {
+
+			vkDestroySemaphore(
+				device,
+				imageAvailableSemaphores[i],
+				nullptr
+			);
+
+			vkDestroyFence(
+				device,
+				inFlightFences[i],
+				nullptr
+			);
+		}
+
+		imageAvailableSemaphores.clear();
+		inFlightFences.clear();
+
+	}
+}
---
+++ lve_uniform.cpp
@@ -0,0 +1,99 @@
+﻿#include"lve_uniform.h"
+
+namespace lve {
+	void LveUniform::createDescriptorSetLayout(VkDevice device) {
+		VkDescriptorSetLayoutBinding uboLayoutBinding{};//创建结构体
+		uboLayoutBinding.binding = 0;//占用描述符集合中的binding0[就是shader里的binding那个]
+		uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;//绑定的是VkBuffer
+		uboLayoutBinding.descriptorCount = 1;//有一个描述符
+		uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;//在顶点着色器阶段使用
+		uboLayoutBinding.pImmutableSamplers = nullptr; //图像采样描述符,暂时默认
+
+		VkDescriptorSetLayoutCreateInfo layoutInfo{};//信息
+		layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+		layoutInfo.bindingCount = 1;
+		layoutInfo.pBindings = &uboLayoutBinding;
+
+		if (vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr, &descriptorSetLayout) != VK_SUCCESS) {//创建
+			throw std::runtime_error("failed to create descriptor set layout!");
+		}
+	}
+	void LveUniform::clean(VkDevice device, uint32_t MAX_FRAMES_IN_FLIGHT) {
+		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+			vkDestroyBuffer(device, uniformBuffers[i], nullptr);
+			vkFreeMemory(device, uniformBuffersMemory[i], nullptr);
+		}
+		vkDestroyDescriptorPool(device, descriptorPool, nullptr);
+		vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
+	}
+	void LveUniform::createUniformBuffer(uint32_t MAX_FRAMES_IN_FLIGHT,LveDevice& device) {
+		VkDeviceSize bufferSize = sizeof(UniformBufferObject);
+
+		//为每一帧创建独立的ubo
+		uniformBuffers.resize(MAX_FRAMES_IN_FLIGHT);
+		uniformBuffersMemory.resize(MAX_FRAMES_IN_FLIGHT);
+		uniformBuffersMapped.resize(MAX_FRAMES_IN_FLIGHT);
+
+		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {//为每个飞行的帧加个ubo
+			device.createBuffer(bufferSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, uniformBuffers[i], uniformBuffersMemory[i]);
+
+			vkMapMemory(device.getDevice(), uniformBuffersMemory[i], 0, bufferSize, 0, &uniformBuffersMapped[i]);//标记位置
+		}
+
+	}
+	void LveUniform::updateUniformBuffer(uint32_t currentImage,VkExtent2D extent, const glm::mat4& modelMatrix, const glm::mat4& view, const glm::mat4& proj) {
+	
+		//UBO
+		UniformBufferObject ubo{};
+		ubo.model = modelMatrix;
+		ubo.view = view;
+		ubo.proj = proj;
+
+		memcpy(uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
+	}
+	void LveUniform::createDescriptorPool(uint32_t MAX_FRAMES_IN_FLIGHT,VkDevice device) {
+		VkDescriptorPoolSize poolSize{};//大小
+		poolSize.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+		poolSize.descriptorCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);//数量
+
+		VkDescriptorPoolCreateInfo poolInfo{};//信息
+		poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+		poolInfo.poolSizeCount = 1;
+		poolInfo.pPoolSizes = &poolSize;
+		poolInfo.maxSets = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);//最大数量
+		poolInfo.flags = 0;//保持默认
+		if (vkCreateDescriptorPool(device, &poolInfo, nullptr, &descriptorPool) != VK_SUCCESS) {//创建
+			throw std::runtime_error("failed to create descriptor pool!");
+		}
+	}
+	void LveUniform::createDescriptorSets(uint32_t MAX_FRAMES_IN_FLIGHT,VkDevice device) {
+		layouts.resize(MAX_FRAMES_IN_FLIGHT,descriptorSetLayout);
+		VkDescriptorSetAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+		allocInfo.descriptorPool = descriptorPool;
+		allocInfo.descriptorSetCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+		allocInfo.pSetLayouts = layouts.data();
+
+		descriptorSets.resize(MAX_FRAMES_IN_FLIGHT);//描述符集
+		if (vkAllocateDescriptorSets(device, &allocInfo, descriptorSets.data()) != VK_SUCCESS) {//创建
+			throw std::runtime_error("failed to allocate descriptor sets!");
+		}
+		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {//填充描述符池
+			VkDescriptorBufferInfo bufferInfo{};
+			bufferInfo.buffer = uniformBuffers[i];
+			bufferInfo.offset = 0;
+			bufferInfo.range = sizeof(UniformBufferObject);
+			VkWriteDescriptorSet descriptorWrite{};
+			descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+			descriptorWrite.dstSet = descriptorSets[i];
+			descriptorWrite.dstBinding = 0;
+			descriptorWrite.dstArrayElement = 0;
+			descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+			descriptorWrite.descriptorCount = 1;
+			descriptorWrite.pBufferInfo = &bufferInfo;
+			descriptorWrite.pImageInfo = nullptr; // Optional
+			descriptorWrite.pTexelBufferView = nullptr; // Optional
+			vkUpdateDescriptorSets(device, 1, &descriptorWrite, 0, nullptr);
+		}
+	}
+}
